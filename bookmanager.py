5af96097caeefe05cf9964c57bb35f88fa15e801
hakevin <37036561+hakevin@users.noreply.github.com>
2019-02-13 14:02:46 -0600

    Add files via upload

--- bookmanager.py
+++ bookmanager.py
@@ -1,25 +1,34 @@
 import os
 
-from flask import Flask
-from flask import redirect
-from flask import render_template
-from flask import request
+from flask import *
 
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import *
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+#project_dir = os.path.dirname(os.path.abspath(__file__))
+#database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+#app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_DATABASE_URI"] ='mysql://dt_admin:abc123@127.0.0.1/dreamteam_db'
 
 db = SQLAlchemy(app)
+
 
 class Book(db.Model):
     title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
 
     def __repr__(self):
         return "<Title: {}>".format(self.title)
+
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
+
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -35,6 +44,7 @@ def home():
     books = Book.query.all()
     return render_template("home.html", books=books)
 
+
 @app.route("/update", methods=["POST"])
 def update():
     try:
@@ -48,14 +58,11 @@ def update():
         print(e)
     return redirect("/")
 
-@app.route("/delete", methods=["POST"])
-def delete():
-    title = request.form.get("title")
-    book = Book.query.filter_by(title=title).first()
-    db.session.delete(book)
-    db.session.commit()
-    return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+#create table book (
+#    -> title string(80)
+#    -> );
